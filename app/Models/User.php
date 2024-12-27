348c88e4b5bc2835d29b5760a18523834628d183
giacomoarienti <giacomo.arienti2@studio.unibo.it>
2024-12-27 16:33:32 +0100

    created Reception method

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,4 +61,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Notification::class);
     }
+
+    public function reception_method(): HasMany
+    {
+        return $this->hasMany(ReceptionMethod::class);
+    }
 }
