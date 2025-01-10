2cb14523295c4fe9dba522cb9fbcf2d1f8c7281e
giacomoarienti <giacomo.arienti2@studio.unibo.it>
2025-01-10 19:30:52 +0100

    app layout and singin view

---
+++ resources/views/auth/signin.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Sign In</h1>
+
+    <form method="POST" action="{{ route('signin') }}">
+        @csrf
+        <div class="form-group has-validation">
+            <label for="email">Email</label>
+            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="Email" value="{{ old('email') }}" required>
+            @error('email')<div class="invalid-feedback">{{ $message }}</div>@enderror
+        </div>
+
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" placeholder="Password" required>
+            @error('password')<div class="invalid-feedback">{{ $message }}</div>@enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Sign In</button>
+    </form>
+@endsection
