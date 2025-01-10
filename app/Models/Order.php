91e1246a2f5a2fe489517c8f3fb65e5c81d5555f
Davide <davide.fiocchi2@studio.unibo.it>
2025-01-10 17:06:06 +0100

    Added Order attributes implementation

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,28 +6,49 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-
+use App\Enums\OrderStatus;
 class Order extends Model
 {
 
     protected $fillable = ["discount"];
 
-    //TODO: attributi derivati
     public function status() : Attribute {
         return Attribute::make(
-            get: fn() => null
+            get: function() {
+                if($this->payment() == null){
+                    return OrderStatus::AWAITING_PAYMENT;
+                }elseif($this->shipping() == null){
+                    return OrderStatus::PENDING;
+                }elseif($this->shipping->status->name == "delivered"){
+                    return OrderStatus::DELIVERED;
+                }else{
+                    return OrderStatus::SHIPPED;
+                }
+            }
         );
     }
 
     public function completed() : Attribute {
         return Attribute::make(
-            get: fn() => null
+            get: fn() => $this->status() == OrderStatus::DELIVERED
         );
     }
 
     public function total() : Attribute {
         return Attribute::make(
-            get: fn() => null
+            get: fn() => $this->coupon == null ? $this->totalBeforeDiscount() : $this->totalBeforeDiscount * (1 - $this->coupon->discount)
+        );
+    }
+
+    public function discount() : Attribute {
+        return Attribute::make(
+            get: fn() => $this->coupon == null ? 0 : $this->totalBeforeDiscount() * $this->coupon->discount
+        );
+    }
+
+    public function totalBeforeDiscount() : Attribute {
+        return Attribute::make(
+            get: fn() => $this->specificProducts->sum(fn($specificProduct) => $specificProduct->pivot->quantity * $specificProduct->price)
         );
     }
 
