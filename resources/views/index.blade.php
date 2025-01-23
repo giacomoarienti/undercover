ffcb8b3ccdc64e2e160ba9c27f0c942ff52cdf04
giacomoarienti <giacomo.arienti2@studio.unibo.it>
2025-01-23 17:28:26 +0100

    index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,43 @@
+@php
+    /** @var \App\Models\User $user */
+    $user = Illuminate\Support\Facades\Auth::user();
+@endphp
+
 @extends('layouts.app')
 
 @section('content')
+    @if($user->is_seller)
+        <div class="container">
+            <div class="row gap-3">
+                <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
+                   href="{{ route('products.index') }}">
+                    Products
+                </a>
 
+                <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
+                   href="{{ route('coupons') }}">
+                    Coupons
+                </a>
+
+                <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
+                   href="{{ route('orders.index') }}">
+                    Orders
+                </a>
+            </div>
+        </div>
+    @else
+        <div class="container">
+            <div class="row gap-3">
+                <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
+                   href="{{ route('products.index') }}">
+                    Shop
+                </a>
+
+                <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
+                   href="{{ route('orders.index') }}">
+                    Orders
+                </a>
+            </div>
+        </div>
+    @endif
 @endsection
