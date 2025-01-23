cabacd01ee7c76f550b0f698336b3a1fdbe03ff2
giacomoarienti <giacomo.arienti2@studio.unibo.it>
2025-01-23 17:02:46 +0100

    accessibility changes

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -7,19 +7,20 @@
     <nav class="container navbar navbar-expand-lg navbar-light bg-light">
         <div class="row h-100 w-100 justify-content-between align-items-center">
             <div class="d-none col-md-3 d-md-inline-flex justify-content-start ps-3" style="max-height: 80px;">
-                <a class="navbar-brand d-none d-md-inline-flex" href="/">
+                <a class="navbar-brand d-none d-md-inline-flex" href="/" title="Home">
                     <img src="{{ Storage::url('public/hat.svg') }}" alt="" style="max-height: 100px; width: 100px">
                 </a>
             </div>
             <div class="col-8 col-md-6 h-100">
                 <div class="d-flex flex-column justify-content-center align-items-center h-100">
                     <a class="navbar-brand d-flex align-items-center m-1 mt-0 ms-3 ms-md-0" href="/">
-                        <img src="{{ Storage::url('public/text.svg') }}" alt="undercover" class="w-100 h-100">
+                        <img src="{{ Storage::url('public/text.svg') }}" alt="" class="w-100 h-100">
                     </a>
                     <form class="d-none d-md-flex justify-content-center align-items-center col-8" action="{{ route('products.index') }}" method="GET" role="search">
                         <div class="input-group rounded-pill">
                             <span class="input-group-text"><i class="fa-solid fa-magnifying-glass"></i></span>
-                            <input type="text" name="search" class="form-control" placeholder="find your new identity" value="{{(isset($filters) and isset($filters['search'])) ? $filters['search'] : ''}}">
+                            <label class="sr-only" for="search-bar">Search product</label>
+                            <input type="text" id="search-bar" name="search" class="form-control" placeholder="find your new identity" value="{{(isset($filters) and isset($filters['search'])) ? $filters['search'] : ''}}">
                         </div>
                     </form>
                 </div>
@@ -41,7 +42,7 @@
                             </li>
                         @endif
                         <li class="nav-item">
-                            <a class="nav-link" href="/notifications">
+                            <a class="nav-link" href="/notifications" title="Notifications">
                                 <i class="fa-regular fa-bell fa-xl ms-3">
                                     @if ($user->unreadNotifications()->count() > 0)
                                         <span class="badge">
@@ -52,7 +53,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('settings') }}">
+                            <a class="nav-link" href="{{ route('settings') }}" title="Account">
                                 <i class="fa-regular fa-user fa-xl ms-3"></i>
                             </a>
                         </li>
