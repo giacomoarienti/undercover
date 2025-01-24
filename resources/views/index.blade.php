26b2a5c93d6a18fc55cad5d4c97a00a5cd480161
giacomoarienti <giacomo.arienti2@studio.unibo.it>
2025-01-24 14:38:43 +0100

    non authenticated user menu in index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,26 +6,7 @@
 @extends('layouts.app')
 
 @section('content')
-    @if($user->is_seller)
-        <div class="container">
-            <div class="row gap-3">
-                <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
-                   href="{{ route('products.index') }}">
-                    Products
-                </a>
-
-                <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
-                   href="{{ route('coupons') }}">
-                    Coupons
-                </a>
-
-                <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
-                   href="{{ route('orders.index') }}">
-                    Orders
-                </a>
-            </div>
-        </div>
-    @else
+    @if(!$user)
         <div class="container">
             <div class="row gap-3">
                 <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
@@ -34,10 +15,45 @@
                 </a>
 
                 <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
-                   href="{{ route('orders.index') }}">
-                    Orders
+                   href="{{ route('auth.signin') }}">
+                    Sign in
                 </a>
             </div>
         </div>
+    @else
+        @if($user->is_seller)
+            <div class="container">
+                <div class="row gap-3">
+                    <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
+                       href="{{ route('products.index') }}">
+                        Products
+                    </a>
+
+                    <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
+                       href="{{ route('coupons') }}">
+                        Coupons
+                    </a>
+
+                    <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
+                       href="{{ route('orders.index') }}">
+                        Orders
+                    </a>
+                </div>
+            </div>
+        @else
+            <div class="container">
+                <div class="row gap-3">
+                    <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
+                       href="{{ route('products.index') }}">
+                        Shop
+                    </a>
+
+                    <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
+                       href="{{ route('orders.index') }}">
+                        Orders
+                    </a>
+                </div>
+            </div>
+        @endif
     @endif
 @endsection
