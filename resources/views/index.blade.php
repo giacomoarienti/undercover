20a57a17c5b377f1c0473f4e0c874045305d889f
Davide <davide.fiocchi2@studio.unibo.it>
2025-01-24 16:40:01 +0100

    Changed homepage appeareance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,54 +6,88 @@
 @extends('layouts.app')
 
 @section('content')
-    @if(!$user)
-        <div class="container">
-            <div class="row gap-3">
-                <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
-                   href="{{ route('products.index') }}">
-                    Shop
-                </a>
-
-                <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
-                   href="{{ route('auth.signin') }}">
-                    Sign in
-                </a>
+    <div class="container">
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="d-none col p-3 pe-0 d-md-flex justify-content-center align-items-center">
+                    <img src="{{ Storage::url('public/logo.svg') }}" alt="logo" class="img-fluid" style="max-height: 35vh;">
+                </div>
+                <div class="col-12 col-md-9">
+                    <div class="card-body d-flex flex-column h-100 justify-content-center align-items-start">
+                        <h1 class="card-title">
+                            The perfect cover
+                        </h1>
+                        <h1 class="card-subtitle h3 mb-3">
+                            for your every secret.
+                        </h1>
+                        <div class="card-text">
+                            Undercover is your go-to destination for unique, high-quality covers. We combine exclusive designs and reliable protection to bring you accessories that reflect your style while safeguarding your devices. Express your personality, without compromise: <em>we've got you covered.</em>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    @else
-        @if($user->is_seller)
-            <div class="container">
-                <div class="row gap-3">
-                    <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
+        <div class="row">
+            @if(!$user)
+                <div class="col-12-sm col-md-6">
+                    <a class="d-flex justify-content-center p-5 bg-primary text-white text-decoration-none h3 text-center rounded"
                        href="{{ route('products.index') }}">
-                        Products
-                    </a>
-
-                    <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
-                       href="{{ route('coupons') }}">
-                        Coupons
-                    </a>
-
-                    <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
-                       href="{{ route('orders.index') }}">
-                        Orders
+                        Browse products
                     </a>
                 </div>
-            </div>
-        @else
-            <div class="container">
-                <div class="row gap-3">
-                    <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
-                       href="{{ route('products.index') }}">
-                        Shop
-                    </a>
 
-                    <a class="col-sm bg-primary text-bg-primary p-5 text-center text-decoration-none h3"
-                       href="{{ route('orders.index') }}">
-                        Orders
+                <div class="col-12-sm col-md-6">
+                    <a class="d-flex justify-content-center p-5 bg-primary text-white text-decoration-none h3 text-center rounded"
+                       href="{{ route('auth.signin') }}">
+                        Sign in
                     </a>
                 </div>
-            </div>
-        @endif
-    @endif
+            @else
+                @if($user->is_seller)
+                    <div class="col-12 col-md-6">
+                        <a class="d-flex justify-content-center p-5 bg-primary text-white text-decoration-none h3 text-center rounded"
+                           href="{{ route('products.index') }}">
+                            Products
+                        </a>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <a class="d-flex justify-content-center p-5 bg-primary text-white text-decoration-none h3 text-center rounded"
+                           href="{{ route('products.create') }}">
+                            New Product
+                        </a>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <a class="d-flex justify-content-center p-5 bg-primary text-white text-decoration-none h3 text-center rounded"
+                           href="{{ route('coupons') }}">
+                            Coupons
+                        </a>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <a class="d-flex justify-content-center p-5 bg-primary text-white text-decoration-none h3 text-center rounded"
+                           href="{{ route('orders.index') }}">
+                            Orders
+                        </a>
+                    </div>
+                @else
+                    <div class="col-12 col-md-6">
+                        <a class="d-flex justify-content-center p-5 bg-primary text-white text-decoration-none h3 text-center rounded"
+                           href="{{ route('products.index') }}">
+                            Shop
+                        </a>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <a class="d-flex justify-content-center p-5 bg-primary text-white text-decoration-none h3 text-center rounded"
+                           href="{{ route('orders.index') }}">
+                            Orders
+                        </a>
+                    </div>
+                @endif
+            @endif
+        </div>
+    </div>
+
 @endsection
