cbde0a6b8814183ff1d00ab016c3bbaf6600c83f
giacomoarienti <giacomo.arienti2@studio.unibo.it>
2025-01-24 16:42:03 +0100

    orders index

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,31 +1,53 @@
+@php
+    function formatTotal(\App\Models\Order $order): string
+    {
+        /** @var \App\Models\User $user */
+        $user = Illuminate\Support\Facades\Auth::user();
+
+        $total = $user->is_seller ? $order->vendorTotal($user) : $order->total;
+        return number_format($total, 2);
+    }
+@endphp
+
 @extends('layouts.app')
 
 @section('content')
 
-<h1>Orders</h1>
-<div class="card">
-    <div class="card-body">
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">Date</th>
-                <th scope="col">Customer</th>
-                <th scope="col">Status</th>
-                <th scope="col">Total</th>
-            </tr>
-            </thead>
-            <tbody>
-                @foreach ($orders as $order)
+    <h1>Orders</h1>
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-hover" aria-label="Orders List">
+                    <thead>
                     <tr>
-                        <td>{{ $order->created_at }}</td>
-                        <td>{{ $order->customer->name }}</td>
-                        <td>{{ $order->status }}</td>
-                        <td>{{ $order->total }}</td>
+                        <th scope="col">Date</th>
+                        <th scope="col">Order ID</th>
+                        <th scope="col">Total</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {{$orders->links()}}
+                    </thead>
+                    <tbody>
+                    @foreach ($orders as $order)
+                        <tr data-order-id="{{ $order->id }}" class="order-row" role="button" tabindex="0">
+                            <td>{{ $order->created_at->format('Y-m-d') }}</td>
+                            <td>{{ $order->id }}</td>
+                            <td>{{ formatTotal($order) }}</td>
+                            <td>{{ ucfirst($order->status?->value) }}</td>
+                            <td>
+                                <a href="{{ route('orders.show', $order) }}" class="btn btn-sm btn-primary view-order"
+                                   title="View order {{ $order->id }} details">
+                                    <i class="fa fa-eye"><span class="sr-only">Order {{ $order->id }} details</span></i>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+
+                {{ $orders->links() }}
+            </div>
+
+        </div>
     </div>
-</div>
 @endsection
